94cbbf10989a0a6ce3de8553ccd0bb147d478899
Rynnel <81760746+Rynnel@users.noreply.github.com>
2023-05-31 13:32:55 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,6 @@
 spisok = list(map(int, input("Введите числа, которые будут входить в список: ").split()))
-spisok[0], spisok[-1] = spisok[-1], spisok[0]
+last = spisok[-1]
+for i in range(len(spisok)-1, 0, -1):
+    spisok[i] = spisok[i-1]
+spisok[0] = last
 print(spisok)
